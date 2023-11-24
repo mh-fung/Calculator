d6fe74728f1cb20add6c4e8a8fce1c118c4b3b89
Hei Fung <heifung@Heis-MacBook-Pro.local>
2023-11-24 20:23:05 +0000

    Added ts for numbers and plus operator

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,62 @@
 import './main.scss'
-const display = document.querySelector<HTMLHeadElement>(".display")
-
-if(!display ) {
+const display = document.querySelector<HTMLInputElement>(".display")
+const plusSign = document.querySelector<HTMLButtonElement>(".button__operator--plus");
+const equalSign = document.querySelector<HTMLButtonElement>(".button__operator--equal");
+const cancel = document.querySelector<HTMLButtonElement>(".button__function--cancel");
+if(!display || !plusSign || !equalSign|| !cancel) {
     throw new Error("Issues with Selector")
 }
-let answer: number = 0;
-let currentNumber: number[] = [];
-
-// const handleClick1 = () => {
-//     const buttonValue = 1
-//     currentNumber.push(buttonValue);
-//     const shown = currentNumber.join("");
-//     display.innerHTML = `${shown}`;
-// };
+let valueFirst: number; 
+let valueSecond: number;
+let stage:number = 0;
+let operator: string;
+let numberFirst: number[] = [];
+let numberSecond: number[] = [];
 
 
 const buttonsNumber = document.querySelectorAll<HTMLElement>(".button__number");
-
 const handleButtonsNumber = (event: Event) => {
     const target = event.currentTarget as HTMLButtonElement;
-    const buttonValue = target.innerText
-    currentNumber.push(Number(buttonValue));
-    display.innerHTML = `${currentNumber.join("")}`;
-}
-
-
+    const buttonValue = target.innerText;
+    if (stage == 0 || stage == 1) {
+        numberFirst.push(Number(buttonValue));
+        display.value = `${numberFirst.join("")}`;
+        valueFirst = Number(display.value);
+    } else {
+        numberSecond.push(Number(buttonValue))
+        numberFirst.push(Number(buttonValue));
+        display.value = `${numberSecond.join("")}`;
+        valueSecond = Number(display.value);
+    }
+};
 buttonsNumber.forEach(button => {
     button.addEventListener("click", handleButtonsNumber)
   });
 
+// idea: all buttons pressed, show up in diplay, perform the calculation
+// operators: store previous number and perform with the next number
+//eg plus: store previous value, if display include +, then + the second value
 
-// idea: all buttons pressed, show up in diplay
-//
+
+const handlePlus = () => {
+    operator = "+"
+    stage = 2
+};
+
+plusSign.addEventListener("click", handlePlus)
+
+
+const handleEqual = () => {
+    if (operator === "+") {
+        const answer = valueFirst + valueSecond;
+        display.value = `${answer}`;
+    };
+};
+
+equalSign.addEventListener("click", handleEqual)
+// const handleCancel = () => {
+//     display.innerText = "0"
+// }
+// cancel.addEventListener("click", handleCancel);
+
 // = : perform calculation and show the answer in the display
